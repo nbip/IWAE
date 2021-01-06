3457063a0cedb4c8e9c523b6fd2e89654edbb84e
nbip <nbip@dtu.dk>
2021-01-06 21:30:52 +0100

    Initial plotting of variational and true posteriors

--- README.md
+++ README.md
@@ -2,15 +2,23 @@
 
 Reproducing results from the [IWAE paper](https://arxiv.org/pdf/1509.00519.pdf) in TensorFlow 2. 
 
+## What can you do with this repo?
+- reproduce the results from the IWAE paper
+- run IWAE models with the DReG estimator
+- get posterior means using * self-normalized importance sampling *
+- draw samples from the true posterior using * sampling importance resampling * (coming)
+
 ## Usage
 The results for the model with 1 stochastic layer and 1, 5 or 50 importance samples can be obtained by running `main.py` with the default settings, adjusting the number of samples.
 ``` 
-python main.py --n_samples  <# of importance samples, 5 by default>  
-               --objective  <choose vae_elbo or iwae_elbo, iwae_elbo by default>
+python main.py --n_samples          <# of importance samples, 5 by default>  
+               --objective          <choose vae_elbo or iwae_elbo, iwae_elbo by default>
+               --stochastic_layers  <# of stochastic layers, 1 by default>
 ```
 The model is investigated further in a series of tasks found in `./tasks`
 
 ## Results
+Samples (left) and mean function (right) when sampling from the prior, during training of an IWAE with 50 importance samples.
 <img src="results/iwae_50.gif" width="600" height="300" />
 
 Test-set log likelihoods are estimated using 5000 importance samples
@@ -29,7 +37,7 @@ Test-set log likelihoods are estimated using 5000 importance samples
 | 5 | -83.48 | -83.89 |
 | 50 | -82.82 | -82.90 |
 
-#### DReG dynamic binarization
+#### DReG estimator, 1 stochastic layer
 The Doubly Reparameterized Gradient Estimator for Monte Carlo Objectives, [DReG](https://arxiv.org/pdf/1810.04152.pdf), provides even lower variance gradients for the inference network, than just using the reparameterization trick. This is implemented in `task02.py`.  
 
 | Method | DReG Test-set LLH | Standard IWAE Test-set LLH |
@@ -42,7 +50,6 @@ The Doubly Reparameterized Gradient Estimator for Monte Carlo Objectives, [DReG]
 `main.py`: original experiment with 1 or 2 stochastic layers.  
 `task01.py`: Use a 2D latent space to investigate both true and variational posteriors. We can use *self-normalized importance sampling* to estimate posterior means and *sampling importance resampling* to draw samples from the true posterior.  
 `task02.py`: Use the Double Reparameterized Gradient Estimator, [DReG](https://arxiv.org/pdf/1810.04152.pdf), to the original experiment.  
-`task03.py`: 2 stochastic layers with 2d and 1d latent spaces
 
 ## Comparisons
 A number of other repositories have reproduced results from the IWAE paper, see for example  
@@ -52,21 +59,7 @@ A number of other repositories have reproduced results from the IWAE paper, see 
 - [xqding](https://github.com/xqding/Importance_Weighted_Autoencoders)
 - [yoonholee](https://github.com/yoonholee/pytorch-vae)
 
-## Settings:  
-
-In the paper the initalizer from [Glorot & Bengeio (2010)](http://proceedings.mlr.press/v9/glorot10a/glorot10a.pdf?source=post_page---------------------------) is used to initialize hidden layers. This is also the keras default initializer, which has also been used here.  
-The paper uses Adam optimizer [Kingma & Ba](https://arxiv.org/abs/1412.6980), with $\beta_1 = 0.9$, $\beta_2=0.999$ and $\epsilon = 10^{-4}$. The default Adam settings in keras, which has $\epsilon=10^{-7}$.
-
-## Issues:
-1 stochastic layer VAE/IWAE: I get test-set LLH 1 nat worse than reported in the paper. If I use warmup I can get the same performance.  
-2 stochastic layer VAE/IWAE: I get worse performance than reported in the paper. What if I use warmup here as well? 
-
-Is using eq 8 and eq 14 for training equivalent? It seems so, I trained task07 using eq 8 and task 11 using eq 14, to get the same marginal llh results. However, you cannot use eq 14 to evaluate the marginal LLH. They are only equivalent in terms of gradients. Well, when $k=1$, they are also equivalent in terms of marginal LLH estimate.  
-
-Note that pytorch dataloaders makes it easier to work with dynamically binarized mnist, see [xqding](https://github.com/xqding/Importance_Weighted_Autoencoders/blob/master/model/vae_models.py)
-
 ## TODO:
-Implement [DReG](https://arxiv.org/abs/1810.04152)  
 Show true and variational posteriors
 Show active units
 Implement quantized distribution  
@@ -76,7 +69,10 @@ Show
 - SIR
 Two layer model with 2 and 1 units, to investigate the marginal distribution in the first stochastic layer.  
 
+Note that pytorch dataloaders makes it easier to work with dynamically binarized mnist, see [xqding](https://github.com/xqding/Importance_Weighted_Autoencoders/blob/master/model/vae_models.py)
+
 ## Done:
+Implement [DReG](https://arxiv.org/abs/1810.04152)  
 Extend to two stochastic layers  
 2 layer:
 - posterior SNIS
@@ -119,12 +115,10 @@ Test-set lower bounds for 1, 5 and 50 importance samples, with DReG and without.
 | 5 |  | -86.46 |
 | 50 |  | -86.35 |
 
-# Implementation learnings
-I could not get the 1-sample VAE/IWAE results to match paper results, they were consistently 1 nat worse. Furthermore, when increasing the number of monte-carlo samples for the VAE, the elbo did not necessarily go down. Initializing the bias of the p(x|z) layer alleviated this issue.  
-The model with two stochastic layers, did not yield similar results to the paper, again, the bias initialization mitigated this issue.
 
 ## Resources:
 https://github.com/yburda/iwae  
+https://arxiv.org/pdf/1509.00519.pdf  
 https://github.com/addtt/ladder-vae-pytorch  
 https://github.com/xqding/Importance_Weighted_Autoencoders  
 https://github.com/xqding/AIWAE  
@@ -142,9 +136,9 @@ https://github.com/neha191091/IWAE/blob/master/iwae/experiments.py
 https://github.com/jmtomczak/vae_vampprior  
 https://github.com/harvardnlp/sa-vae  
 https://arxiv.org/pdf/1602.02282.pdf  
-https://arxiv.org/pdf/1509.00519.pdf  
+https://arxiv.org/pdf/1802.04537.pdf  
+https://arxiv.org/pdf/1810.04152.pdf  
 https://arxiv.org/pdf/1902.02102.pdf  
-https://arxiv.org/pdf/1802.04537.pdf
 
 
 ### Two stochastic layers
@@ -154,4 +148,4 @@ See
 - [addtt](https://github.com/addtt/ladder-vae-pytorch)
 - [Ladder VAE](https://arxiv.org/pdf/1602.02282.pdf) and accompanying [github](https://github.com/casperkaae/LVAE)
 
-[xqding](https://github.com/xqding/Importance_Weighted_Autoencoders) and [ShwanMario](https://github.com/ShwanMario/IWAE) train IWAEs with two stochastic layers, both without reaching Yburdas results, using a different training setup than me.  
+[xqding](https://github.com/xqding/Importance_Weighted_Autoencoders) and [ShwanMario](https://github.com/ShwanMario/IWAE) train IWAEs with two stochastic layers, both without reaching Yburdas results, using a different training setup than here.  
--- tasks/task01.py
+++ tasks/task01.py
@@ -1,29 +1,31 @@
 import tensorflow as tf
+from tensorflow_probability import distributions as tfd
 from tensorflow import keras
 import numpy as np
 import os
 import argparse
 import datetime
 import time
-import matplotlib
-matplotlib.use('Agg')  # needed when running from commandline
-import matplotlib.pyplot as plt
 import sys
-sys.path.insert(0, '../src')
+sys.path.insert(0, './src')
 import utils
-import model
+import iwae1
+import iwae2
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_latent", type=int, default=50, help="number of latent space dimensions")
+parser.add_argument("--stochastic_layers", type=int, default=1, choices=[1, 2], help="number of stochastic layers in the model")
 parser.add_argument("--n_samples", type=int, default=5, help="number of importance samples")
-parser.add_argument("--n_hidden", type=int, default=200, help="number of hidden units in the NN layers")
 parser.add_argument("--batch_size", type=int, default=20, help="batch size")
 parser.add_argument("--epochs", type=int, default=-1,
                     help="numper of epochs, if set to -1 number of epochs "
                          "will be set based on the learning rate scheme from the paper")
+parser.add_argument("--objective", type=str, default="iwae_elbo", choices=["vae_elbo", "iwae_elbo", "iwae_eq14", "vae_elbo_kl"])
 parser.add_argument("--gpu", type=str, default='0', help="Choose GPU")
 args = parser.parse_args()
 print(args)
+
+# ---- string describing the experiment, to use in tensorboard and plots
+string = "task01_{0}_{1}_{2}".format(args.objective, args.stochastic_layers, args.n_samples)
 
 # ---- set the visible GPU devices
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
@@ -33,15 +35,6 @@ gpus = tf.config.list_physical_devices('GPU')
 if gpus:
     tf.config.experimental.set_memory_growth(gpus[0], True)
 
-# ---- plot settings
-plt.rcParams["font.family"] = "serif"
-plt.rcParams['font.size'] = 15.0
-plt.rcParams['axes.spines.right'] = False
-plt.rcParams['axes.spines.top'] = False
-plt.rcParams['savefig.format'] = 'pdf'
-plt.rcParams['lines.linewidth'] = 2.5
-plt.rcParams['figure.autolayout'] = True
-
 # ---- set random seeds
 np.random.seed(123)
 tf.random.set_seed(123)
@@ -49,24 +42,17 @@ tf.random.set_seed(123)
 # ---- number of passes over the data, see bottom of page 6 in [1]
 if args.epochs == -1:
     epochs = 0
-    learning_rate_change_epoch = []
-    learning_rates = []
+    learning_rate_dict = {}
 
     for i in range(8):
-        learning_rates.append(0.001 * 10**(-i/7))
+        learning_rate = 0.001 * 10**(-i/7)
+        learning_rate_dict[epochs] = learning_rate
         epochs += 3 ** i
-        learning_rate_change_epoch.append(epochs)
 
 else:
     epochs = args.epochs
-    learning_rates = []
-    learning_rates.append(0.0001)
-
-# ---- experiment settings
-n_latent = args.n_latent
-n_samples = args.n_samples
-n_hidden = args.n_hidden
-batch_size = args.batch_size
+    learning_rate_dict = {}
+    learning_rate_dict[0] = 0.0001
 
 # ---- load data
 (Xtrain, ytrain), (Xtest, ytest) = keras.datasets.mnist.load_data()
@@ -77,57 +63,67 @@ Ntest = Xtest.shape[0]
 Xtrain = Xtrain.reshape(Ntrain, -1) / 255
 Xtest = Xtest.reshape(Ntest, -1) / 255
 
-# ---- train step
-@tf.function
-def train_step(model, x, n_samples, optimizer):
-
-    with tf.GradientTape() as tape:
-        res = model(x, n_samples)
-        loss = res["loss"]
-
-    grads = tape.gradient(loss, model.trainable_weights)
-    optimizer.apply_gradients(zip(grads, model.trainable_weights))
-
-    return res
-
-
-# ---- prepare tensorboard
-current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-train_log_dir = "/tmp/iwae/task01/" + current_time + "/train"
-train_summary_writer = tf.summary.create_file_writer(train_log_dir)
-
-# ---- prepare the data
-Ntrain = Xtrain.shape[0]
-Ntest = Xtest.shape[0]
-
+# ---- experiment settings
+objective = args.objective
+n_samples = args.n_samples
+batch_size = args.batch_size
 steps_pr_epoch = Ntrain // batch_size
 total_steps = steps_pr_epoch * epochs
 
-# ---- instantiate the model, optimizer and metrics
-model = model.VAE(n_hidden, n_latent)
+# ---- prepare tensorboard
+current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+train_log_dir = "/tmp/iwae/{0}/".format(string) + current_time + "/train"
+train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+test_log_dir = "/tmp/iwae/{0}/".format(string) + current_time + "/test"
+test_summary_writer = tf.summary.create_file_writer(test_log_dir)
 
-optimizer = keras.optimizers.Adam(learning_rates[0])
+# ---- instantiate the model, optimizer and metrics
+if args.stochastic_layers == 1:
+    n_latent = [2]
+    n_hidden = [200]
+    model = iwae1.IWAE(n_hidden[0], n_latent[0])
+else:
+    n_latent = [2, 2]
+    n_hidden = [200, 100]
+    model = iwae2.IWAE(n_hidden, n_latent)
+
+optimizer = keras.optimizers.Adam(learning_rate_dict[0], epsilon=1e-4)
 print("Initial learning rate: ", optimizer.learning_rate.numpy())
+
+# ---- prepare plotting of samples during training
+# use the same samples from the prior throughout training
+pz = tfd.Normal(0, 1)
+z = pz.sample([100, n_latent[-1]])
+
+plt_epochs = list(2**np.arange(12))
+plt_epochs.insert(0, 0)
+plt_epochs.append(epochs-1)
+
+# ---- binarize the test data
+# we'll only do this once, while the training data is binarized at the
+# start of each epoch
+Xtest = utils.bernoullisample(Xtest)
 
 # ---- do the training
 start = time.time()
 best = float(-np.inf)
 
-train_history = []
-
-# ---- binarize the training data once (and not at the start of each epoch)
-Xtrain_binarized = utils.bernoullisample(Xtrain)
-
-train_dataset = (tf.data.Dataset.from_tensor_slices(Xtrain_binarized)
-        .shuffle(Ntrain).batch(batch_size))
-
 for epoch in range(epochs):
 
-    # ---- check if the learning rate needs to be updated
-    if args.epochs == -1 and np.sum(epoch == np.asarray(learning_rate_change_epoch)) > 0:
-        idx = np.where(epoch == np.asarray(learning_rate_change_epoch))[0][0]
+    # ---- binarize the training data at the start of each epoch
+    Xtrain_binarized = utils.bernoullisample(Xtrain)
 
-        new_learning_rate = learning_rates[idx + 1]
+    train_dataset = (tf.data.Dataset.from_tensor_slices(Xtrain_binarized)
+        .shuffle(Ntrain).batch(batch_size))
+
+    # ---- plot samples from the prior at this epoch
+    if epoch in plt_epochs:
+        model.generate_and_save_images(z, epoch, string)
+        model.generate_and_save_posteriors(Xtest, ytest, 10, epoch, string)
+
+    # ---- check if the learning rate needs to be updated
+    if args.epochs == -1 and epoch in learning_rate_dict:
+        new_learning_rate = learning_rate_dict[epoch]
         old_learning_rate = optimizer.learning_rate.numpy()
 
         print("Changing learning rate from {0} to {1}".format(old_learning_rate, new_learning_rate))
@@ -136,32 +132,46 @@ for epoch in range(epochs):
     for _step, x_batch in enumerate(train_dataset):
         step = _step + steps_pr_epoch * epoch
 
+        # ---- warm-up
+        beta = 1.0
+        # beta = np.min([step / 200000, 1.0]).astype(np.float32)
+
         # ---- one training step
-        res = train_step(model, x_batch, n_samples, optimizer)
+        res = model.train_step(x_batch, n_samples, beta, optimizer, objective=objective)
 
         if step % 200 == 0:
-            train_history.append(res["elbo"].numpy().mean())
+
+            # ---- write training stats to tensorboard
+            with train_summary_writer.as_default():
+                model.write_to_tensorboard(res, step)
+
+            # ---- monitor the test-set
+            test_res = model.val_step(Xtest, n_samples, beta)
+
+            # ---- write test stats to tensorboard
+            with test_summary_writer.as_default():
+                model.write_to_tensorboard(test_res, step)
 
             took = time.time() - start
             start = time.time()
 
-            print("epoch {0}/{1}, step {2}/{3}, train ELBO: {4:.2f}, time: {5:.2f}"
-                  .format(epoch, epochs, step, total_steps, res["elbo"].numpy(), took))
+            print("epoch {0}/{1}, step {2}/{3}, train ELBO: {4:.2f}, val ELBO: {5:.2f}, time: {6:.2f}"
+                  .format(epoch, epochs, step, total_steps, res[objective].numpy(), test_res[objective], took))
 
 # ---- save final weights
-model.save_weights('/tmp/iwae/task01/final_weights' + '_nsamples_{}'.format(n_samples))
+model.save_weights('/tmp/iwae/{0}/final_weights'.format(string))
+
+# ---- load the final weights?
+# model.load_weights('/tmp/iwae/{0}/final_weights'.format(string))
 
 # ---- test-set llh estimate using 5000 samples
 test_elbo_metric = utils.MyMetric()
 L = 5000
 
-# ---- binarize Xtest
-Xtest = utils.bernoullisample(Xtest)
-
 # ---- since we are using 5000 importance samples we have to loop over each element of the test-set
 for i, x in enumerate(Xtest):
     res = model(x[None, :].astype(np.float32), L)
-    test_elbo_metric.update_state(res['elbo'][None, None])
+    test_elbo_metric.update_state(res['iwae_elbo'][None, None])
     if i % 200 == 0:
         print("{0}/{1}".format(i, Ntest))
 
@@ -170,9 +180,146 @@ test_elbo_metric.reset_states()
 
 print("Test-set {0} sample log likelihood estimate: {1:.4f}".format(L, test_set_llh))
 
-# ---- plot samples from the prior
-utils.plot_prior(model, n=10,
-                 epoch=step // steps_pr_epoch,
-                 n_latent=n_latent,
-                 prefix='task01_',
-                 suffix='_nsamples_{}'.format(n_samples))
+# TODO: compare the variational posterior to the true posterior over a grid
+import tensorflow as tf
+import tensorflow.keras as keras
+import matplotlib
+matplotlib.use('Agg')  # needed when running from commandline
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+from scipy.special import logsumexp
+
+model.load_weights('/tmp/iwae/{0}/final_weights'.format(string))
+
+
+def get_grid(range1, range2, n1, n2):
+    x = np.linspace(range1[0], range1[1], n1)
+    y = np.linspace(range2[0], range2[1], n2)
+
+    X, Y = np.meshgrid(x, y)
+    grid = np.asarray([X.reshape(n1 * n2), Y.reshape(n1 * n2)]).T
+
+    return grid, X, Y
+
+
+# ---- fontsize and markersize
+fs = 35
+ms = 100
+n_examples = 10
+
+# ---- get some results on the test-set
+res = model(Xtest, 20)
+
+z, QZX = model.encoder(Xtest, 20)
+
+# ---- prepare contours over a grid
+n1 = 200
+n2 = 200
+range_ = [-3, 3]
+
+grid, X, Y = get_grid(range_, range_, n1, n2)
+
+# ---- prior over the grid
+prior = multivariate_normal(np.zeros(n_latent), np.ones(n_latent))
+prior_pdf = prior.pdf(grid).reshape(n2, n1)
+
+for i in range(n_examples):
+
+    # ---- test-data
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.imshow(Xtest[i, :].reshape(28, 28), cmap='gray_r')
+    ax.axis('off')
+    plt.savefig('{0}_digit_{1}'.format(string, i))
+    plt.close()
+
+    # ---- new grid scaled to the variational posterior
+    qzx = QZX[i, :]
+
+    scale = 3
+    range1 = [(qzx.loc[0] - scale * qzx.scale[0]).numpy(), (qzx.loc[0] + scale * qzx.scale[0]).numpy()]
+    range2 = [(qzx.loc[1] - scale * qzx.scale[1]).numpy(), (qzx.loc[1] + scale * qzx.scale[1]).numpy()]
+
+    grid1, X1, Y1 = get_grid(range1, range2, n1, n2)
+
+    variational_posterior = np.exp(tf.reduce_sum(qzx.log_prob(grid1), axis=-1).numpy().reshape(n2, n1))
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    ppr = ax.contour(X, Y, prior_pdf, 5)
+    ppo = ax.contour(X1, Y1, variational_posterior, 5, cmap='RdGy_r', linewidths=1)
+    ax.axis('equal')
+    plt.savefig('{0}_variational_posterior_{1}'.format(string, i))
+    plt.close()
+
+    # ---- reconstructions
+    logits, pxz = model.decoder(z[:, i, :][:, None, :])
+    x_rec = pxz.sample().numpy().squeeze()
+
+    # put the original digit first
+    canvas = np.zeros((28, 28 * (1 + x_rec.shape[0])))
+    canvas[:, :28] = Xtest[i, :].reshape(28, 28)
+
+    for j in range(1, x_rec.shape[0] + 1):
+        canvas[:, j * 28: (j + 1) * 28] = x_rec[j - 1, :].reshape(28, 28)
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.imshow(canvas, cmap='gray_r')
+    ax.axis('off')
+    plt.savefig('{0}_reconstructions_{1}'.format(string, i))
+    plt.close()
+
+    # ---- inspect the true posterior
+    # prior p(z)
+    lpz = prior.logpdf(grid1)
+
+    # observation model p(x|z) over the grid
+    _, observation_model = model.decoder(grid1[None, :, :])
+
+    lpxz = tf.reduce_sum(observation_model.log_prob(Xtest[i, :]), axis=-1).numpy().squeeze()
+
+    # log posterior and normalization
+    log_posterior = lpxz + lpz
+
+    log_posterior = log_posterior - logsumexp(log_posterior)
+
+    log_posterior = log_posterior.reshape(n2, n1)
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.contour(X, Y, prior_pdf, 5)
+    ax.contour(X1, Y1, np.exp(log_posterior), 5, cmap='RdGy_r', linewidths=1)
+    ax.axis('equal')
+    plt.savefig('{0}_true_posterior_{1}'.format(string, i))
+    plt.close()
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.contour(X1, Y1, variational_posterior, 5, colors='black', linewidths=1)
+    ax.contour(X1, Y1, np.exp(log_posterior), 5, cmap='RdGy_r', linewidths=1)
+    ax.axis('equal')
+    # get axis limits for last plot
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    plt.savefig('{0}_posteriors_{1}'.format(string, i))
+    plt.close()
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.contour(X, Y, prior_pdf, 5)
+    ax.contour(X1, Y1, variational_posterior, 5, colors='black', linewidths=1)
+    ax.contour(X1, Y1, np.exp(log_posterior), 5, cmap='RdGy_r', linewidths=1)
+    ax.axis('equal')
+    plt.savefig('{0}_posteriorss_{1}'.format(string, i))
+    plt.close()
+
+    plt.clf()
+    fig, ax = plt.subplots()
+    ax.contour(X, Y, prior_pdf, 5)
+    ax.contour(X1, Y1, np.exp(log_posterior), 5, cmap='RdGy_r', linewidths=1)
+    ax.axis('equal')
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    plt.savefig('{0}_posteriorsss_{1}'.format(string, i))
+    plt.close()
